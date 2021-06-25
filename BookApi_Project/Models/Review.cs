041f57584547f4ac8316db1c68b90a8ab56eaebd
kindeme <fkounoudji@gmail.com>
2021-06-25 15:47:40 -0500

    Add project files.

---
+++ BookApi_Project/Models/Review.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookApi_Project.Models
+{
+    public class Review
+    {
+        public int Id { get; set; }
+        public string HeadLine { get; set; }
+        public string ReviewText { get; set; }
+        public int Rating { get; set; }
+        public virtual Reviewer Reviewer { get; set; }
+        public virtual Book Book { get; set; }
+
+    }
+}
+    
